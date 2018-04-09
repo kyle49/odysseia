10c058fc4b92e1083c0236f0b33a3bc6331f003d
kyle49 <xinakai@hotmail.com>
2018-04-09 18:11:24 +0800

    Second Update

--- probInformTrading.R
+++ probInformTrading.R
@@ -8,7 +8,7 @@ lkhd_bsc = function(B, S, a, d, mu, eb, es){
   # eb is the noise buy arrival rate
   # es is the noise sell arrival rate
   
-  (1-a)*dpois(B, eb)*dpois(S, es) + a*d*dpois(B, eb)*dpois(S, es + mu) + a*(1-d)*dpois(B, eb + mu)*dpois(S, es) 
+  (1-a)*dpois(B, eb)*dpois(S, es) + a*(d*dpois(B, eb)*dpois(S, es + mu) + (1-d)*dpois(B, eb + mu)*dpois(S, es)) 
 }
 
 PIN_bsc = function(a, mu, eb, es){
@@ -37,17 +37,21 @@ simu_bsc = function(a, d, mu, eb, es){
 
 
 ## Simulation of test data ##
+
 set.seed(2018)
 out = replicate(100, simu_bsc(0.1, 0.5, 50, 100, 120))
 BS = data.frame(B = out[1,], S = out[2,])
 
-## Estimate paras. by JAGS
+## Estimate paras. by JAGS ##
+
 library(R2WinBUGS)
 library(rjags)
 model_bsc = function(){
   for(i in 1:100){
-    B[i] ~ dpois(eb + mu * a * (1-d))
-    S[i] ~ dpois(es + mu * a * d)
+    E[i] ~ dbern(a)
+    N[i] ~ dbern(d)
+    B[i] ~ dpois(eb + mu * E[i] * (1-N[i]))
+    S[i] ~ dpois(es + mu * E[i] * N[i])
   }
   a ~ dunif(0,1)
   d ~ dunif(0,1)
@@ -62,4 +66,32 @@ mod.vars = c("a", "d", "eb", "es", "mu")
 mod.sim = coda.samples(mod.jags, mod.vars, n.iter = 10000)
 summary(mod.sim)
 plot(mod.sim)
-## Does not converge in 10000 iterations
+
+## Estimate paras. by EM ##
+
+iter = 10000
+
+# initialize
+
+set.seed(409)
+a = 0.1
+d = 0.5
+Z = rbinom(100, 1, a)
+N = rbinom(100, 1, d)
+mu = runif(0,100)
+eb = runif(0,200)
+es = runif(0,200)
+
+for(i in 1:iter){
+  
+  # P(E|theta, X) = P(theta, X|E) * P(E) / P(theta, X)
+  # hence P(E = j|theta, X) = P(E = j) * P(theta, X|E = j) / Sum 
+  # Sum = P(E = 0) * P(theta, X|E = 0) + P(E = 1) * P(theta, X|E = 1)
+  # P(theta, X|E = 1) = P(theta, X|N = 0) + P(theta, X|N = 1)
+  
+  p = 0
+  for(j in 1:100){
+    
+    p = p + l * (a * Z[i] + (1-a) * (1-Z[i])) * (d * N[i] + (1-d)*(1-N[i]))
+  }
+}
